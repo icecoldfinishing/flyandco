6ce32dc6a306de23b491c85c5bef461629dc50f2
icecoldfinishing <rohyamboara@gmail.com>
2026-01-15 13:41:09 +0300

    ok

--- src/bd/00_script.sql
+++ src/bd/00_script.sql
@@ -247,11 +247,11 @@ VALUES
 -- =========================
 INSERT INTO prix_vol (id_vol, classe, prix)
 VALUES
-(1, 'ECONOMY', 350000),
-(1, 'BUSINESS', 750000),
+(1, 'ECONOMY', 700000),
+(1, 'FIRST', 1200000),
 (2, 'ECONOMY', 330000),
-(2, 'BUSINESS', 700000),
-(3, 'ECONOMY', 280000);
+(2, 'FIRST', 1200000),
+(3, 'ECONOMY', 700000);
 
 
 -- =========================
@@ -288,3 +288,161 @@ JOIN vol v ON vi.id_vol = v.id_vol
 JOIN avion a ON vi.id_avion = a.id_avion
 JOIN compagnie c ON a.id_compagnie = c.id_compagnie
 ORDER BY c.id_compagnie, vi.date_depart, v.id_vol;
+
+
+-- FIRST CLASS (1A → 5B = 10 sièges)
+INSERT INTO siege (id_avion, numero, classe)
+VALUES
+(1,'1A','FIRST'),(1,'1B','FIRST'),
+(1,'2A','FIRST'),(1,'2B','FIRST'),
+(1,'3A','FIRST'),(1,'3B','FIRST'),
+(1,'4A','FIRST'),(1,'4B','FIRST'),
+(1,'5A','FIRST'),(1,'5B','FIRST');
+-- ECONOMY (6A → 35B = 60 sièges)
+INSERT INTO siege (id_avion, numero, classe)
+SELECT 1, concat(n,'A'), 'ECONOMY' FROM generate_series(6,35) n
+UNION ALL
+SELECT 1, concat(n,'B'), 'ECONOMY' FROM generate_series(6,35);
+
+
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(1,'1A','FIRST'), (1,'1B','FIRST'),
+(1,'2A','FIRST'), (1,'2B','FIRST'),
+(1,'3A','FIRST'), (1,'3B','FIRST'),
+(1,'4A','FIRST'), (1,'4B','FIRST'),
+(1,'5A','FIRST'), (1,'5B','FIRST');
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(1,'6A','ECONOMY'), (1,'6B','ECONOMY'),
+(1,'7A','ECONOMY'), (1,'7B','ECONOMY'),
+(1,'8A','ECONOMY'), (1,'8B','ECONOMY'),
+(1,'9A','ECONOMY'), (1,'9B','ECONOMY'),
+(1,'10A','ECONOMY'), (1,'10B','ECONOMY'),
+(1,'11A','ECONOMY'), (1,'11B','ECONOMY'),
+(1,'12A','ECONOMY'), (1,'12B','ECONOMY'),
+(1,'13A','ECONOMY'), (1,'13B','ECONOMY'),
+(1,'14A','ECONOMY'), (1,'14B','ECONOMY'),
+(1,'15A','ECONOMY'), (1,'15B','ECONOMY'),
+(1,'16A','ECONOMY'), (1,'16B','ECONOMY'),
+(1,'17A','ECONOMY'), (1,'17B','ECONOMY'),
+(1,'18A','ECONOMY'), (1,'18B','ECONOMY'),
+(1,'19A','ECONOMY'), (1,'19B','ECONOMY'),
+(1,'20A','ECONOMY'), (1,'20B','ECONOMY'),
+(1,'21A','ECONOMY'), (1,'21B','ECONOMY'),
+(1,'22A','ECONOMY'), (1,'22B','ECONOMY'),
+(1,'23A','ECONOMY'), (1,'23B','ECONOMY'),
+(1,'24A','ECONOMY'), (1,'24B','ECONOMY'),
+(1,'25A','ECONOMY'), (1,'25B','ECONOMY'),
+(1,'26A','ECONOMY'), (1,'26B','ECONOMY'),
+(1,'27A','ECONOMY'), (1,'27B','ECONOMY'),
+(1,'28A','ECONOMY'), (1,'28B','ECONOMY'),
+(1,'29A','ECONOMY'), (1,'29B','ECONOMY'),
+(1,'30A','ECONOMY'), (1,'30B','ECONOMY'),
+(1,'31A','ECONOMY'), (1,'31B','ECONOMY'),
+(1,'32A','ECONOMY'), (1,'32B','ECONOMY'),
+(1,'33A','ECONOMY'), (1,'33B','ECONOMY'),
+(1,'34A','ECONOMY'), (1,'34B','ECONOMY'),
+(1,'35A','ECONOMY'), (1,'35B','ECONOMY');
+
+
+
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(2,'1A','FIRST'), (2,'1B','FIRST'),
+(2,'2A','FIRST'), (2,'2B','FIRST'),
+(2,'3A','FIRST'), (2,'3B','FIRST'),
+(2,'4A','FIRST'), (2,'4B','FIRST');
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(2,'5A','ECONOMY'), (2,'5B','ECONOMY'),
+(2,'6A','ECONOMY'), (2,'6B','ECONOMY'),
+(2,'7A','ECONOMY'), (2,'7B','ECONOMY'),
+(2,'8A','ECONOMY'), (2,'8B','ECONOMY'),
+(2,'9A','ECONOMY'), (2,'9B','ECONOMY'),
+(2,'10A','ECONOMY'), (2,'10B','ECONOMY'),
+(2,'11A','ECONOMY'), (2,'11B','ECONOMY'),
+(2,'12A','ECONOMY'), (2,'12B','ECONOMY'),
+(2,'13A','ECONOMY'), (2,'13B','ECONOMY'),
+(2,'14A','ECONOMY'), (2,'14B','ECONOMY'),
+(2,'15A','ECONOMY'), (2,'15B','ECONOMY'),
+(2,'16A','ECONOMY'), (2,'16B','ECONOMY'),
+(2,'17A','ECONOMY'), (2,'17B','ECONOMY'),
+(2,'18A','ECONOMY'), (2,'18B','ECONOMY'),
+(2,'19A','ECONOMY'), (2,'19B','ECONOMY'),
+(2,'20A','ECONOMY'), (2,'20B','ECONOMY'),
+(2,'21A','ECONOMY'), (2,'21B','ECONOMY'),
+(2,'22A','ECONOMY'), (2,'22B','ECONOMY'),
+(2,'23A','ECONOMY'), (2,'23B','ECONOMY'),
+(2,'24A','ECONOMY'), (2,'24B','ECONOMY');
+
+
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(3,'1A','FIRST'), (3,'1B','FIRST'),
+(3,'2A','FIRST'), (3,'2B','FIRST'),
+(3,'3A','FIRST'), (3,'3B','FIRST'),
+(3,'4A','FIRST'), (3,'4B','FIRST'),
+(3,'5A','FIRST'), (3,'5B','FIRST'),
+(3,'6A','FIRST'), (3,'6B','FIRST'),
+(3,'7A','FIRST'), (3,'7B','FIRST'),
+(3,'8A','FIRST'), (3,'8B','FIRST'),
+(3,'9A','FIRST'), (3,'9B','FIRST'),
+(3,'10A','FIRST'), (3,'10B','FIRST');
+INSERT INTO siege (id_avion, numero, classe) VALUES
+(3,'11A','ECONOMY'), (3,'11B','ECONOMY'),
+(3,'12A','ECONOMY'), (3,'12B','ECONOMY'),
+(3,'13A','ECONOMY'), (3,'13B','ECONOMY'),
+(3,'14A','ECONOMY'), (3,'14B','ECONOMY'),
+(3,'15A','ECONOMY'), (3,'15B','ECONOMY'),
+(3,'16A','ECONOMY'), (3,'16B','ECONOMY'),
+(3,'17A','ECONOMY'), (3,'17B','ECONOMY'),
+(3,'18A','ECONOMY'), (3,'18B','ECONOMY'),
+(3,'19A','ECONOMY'), (3,'19B','ECONOMY'),
+(3,'20A','ECONOMY'), (3,'20B','ECONOMY'),
+(3,'21A','ECONOMY'), (3,'21B','ECONOMY'),
+(3,'22A','ECONOMY'), (3,'22B','ECONOMY'),
+(3,'23A','ECONOMY'), (3,'23B','ECONOMY'),
+(3,'24A','ECONOMY'), (3,'24B','ECONOMY'),
+(3,'25A','ECONOMY'), (3,'25B','ECONOMY'),
+(3,'26A','ECONOMY'), (3,'26B','ECONOMY'),
+(3,'27A','ECONOMY'), (3,'27B','ECONOMY'),
+(3,'28A','ECONOMY'), (3,'28B','ECONOMY'),
+(3,'29A','ECONOMY'), (3,'29B','ECONOMY'),
+(3,'30A','ECONOMY'), (3,'30B','ECONOMY'),
+(3,'31A','ECONOMY'), (3,'31B','ECONOMY'),
+(3,'32A','ECONOMY'), (3,'32B','ECONOMY'),
+(3,'33A','ECONOMY'), (3,'33B','ECONOMY'),
+(3,'34A','ECONOMY'), (3,'34B','ECONOMY'),
+(3,'35A','ECONOMY'), (3,'35B','ECONOMY'),
+(3,'36A','ECONOMY'), (3,'36B','ECONOMY'),
+(3,'37A','ECONOMY'), (3,'37B','ECONOMY'),
+(3,'38A','ECONOMY'), (3,'38B','ECONOMY'),
+(3,'39A','ECONOMY'), (3,'39B','ECONOMY'),
+(3,'40A','ECONOMY'), (3,'40B','ECONOMY'),
+(3,'41A','ECONOMY'), (3,'41B','ECONOMY'),
+(3,'42A','ECONOMY'), (3,'42B','ECONOMY'),
+(3,'43A','ECONOMY'), (3,'43B','ECONOMY'),
+(3,'44A','ECONOMY'), (3,'44B','ECONOMY'),
+(3,'45A','ECONOMY'), (3,'45B','ECONOMY'),
+(3,'46A','ECONOMY'), (3,'46B','ECONOMY'),
+(3,'47A','ECONOMY'), (3,'47B','ECONOMY'),
+(3,'48A','ECONOMY'), (3,'48B','ECONOMY'),
+(3,'49A','ECONOMY'), (3,'49B','ECONOMY'),
+(3,'50A','ECONOMY'), (3,'50B','ECONOMY'),
+(3,'51A','ECONOMY'), (3,'51B','ECONOMY'),
+(3,'52A','ECONOMY'), (3,'52B','ECONOMY'),
+(3,'53A','ECONOMY'), (3,'53B','ECONOMY'),
+(3,'54A','ECONOMY'), (3,'54B','ECONOMY'),
+(3,'55A','ECONOMY'), (3,'55B','ECONOMY'),
+(3,'56A','ECONOMY'), (3,'56B','ECONOMY'),
+(3,'57A','ECONOMY'), (3,'57B','ECONOMY'),
+(3,'58A','ECONOMY'), (3,'58B','ECONOMY'),
+(3,'59A','ECONOMY'), (3,'59B','ECONOMY'),
+(3,'60A','ECONOMY'), (3,'60B','ECONOMY'),
+(3,'61A','ECONOMY'), (3,'61B','ECONOMY'),
+(3,'62A','ECONOMY'), (3,'62B','ECONOMY'),
+(3,'63A','ECONOMY'), (3,'63B','ECONOMY'),
+(3,'64A','ECONOMY'), (3,'64B','ECONOMY'),
+(3,'65A','ECONOMY'), (3,'65B','ECONOMY'),
+(3,'66A','ECONOMY'), (3,'66B','ECONOMY'),
+(3,'67A','ECONOMY'), (3,'67B','ECONOMY'),
+(3,'68A','ECONOMY'), (3,'68B','ECONOMY'),
+(3,'69A','ECONOMY'), (3,'69B','ECONOMY'),
+(3,'70A','ECONOMY'), (3,'70B','ECONOMY');
+
